dc068cbe209ddd6c53fb85aa56a525a7680e63fc
Jiaks <160535015+Jiaks95@users.noreply.github.com>
2025-06-08 20:03:06 +0200

    Revert "next.config.mjs" to the previous version

--- next.config.mjs
+++ next.config.mjs
@@ -1,61 +1,8 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     images: {
-//         domains: ['indigo-secret-viper-132.mypinata.cloud']
-//     }
-// };
-
-// export default nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['indigo-secret-viper-132.mypinata.cloud'],
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: 
-              "default-src 'self'; " +
-              "script-src 'self'; " +
-              "object-src 'none'; " +
-              "frame-ancestors 'none'; " +
-              "style-src 'self' 'unsafe-inline'; " +
-              "img-src 'self' data: https:; " +
-              "font-src 'self' https: data:;",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "Referrer-Policy",
-            value: "no-referrer",
-          },
-          {
-            key: "Strict-Transport-Security",
-            value: "max-age=63072000; includeSubDomains; preload",
-          },
-          {
-            key: "Permissions-Policy",
-            value: "camera=(), microphone=(), geolocation=()",
-          },
-          {
-            key: "Cross-Origin-Resource-Policy",
-            value: "same-site",
-          },
-        ],
-      },
-    ];
-  },
+    images: {
+        domains: ['indigo-secret-viper-132.mypinata.cloud']
+    }
 };
 
 export default nextConfig;
