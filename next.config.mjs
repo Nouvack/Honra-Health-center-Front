f88c07177f25bc4d4a51790b4b4a9a28b446bdc2
Jiaks <160535015+Jiaks95@users.noreply.github.com>
2025-06-09 12:07:11 +0200

    Delete faulty CSP from headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,62 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//         domains: ['indigo-secret-viper-132.mypinata.cloud']
+//     },
+//   async headers() {
+//     return [
+//       {
+//         source: "/(.*)",
+//         headers: [
+//           {
+//             key: "X-Frame-Options",
+//             value: "DENY",
+//           },
+//           {
+//             key: "X-Content-Type-Options",
+//             value: "nosniff",
+//           },
+//           {
+//             key: "Referrer-Policy",
+//             value: "no-referrer",
+//           },
+//           {
+//             key: "Strict-Transport-Security",
+//             value: "max-age=63072000; includeSubDomains; preload",
+//           },
+//           {
+//             key: "Permissions-Policy",
+//             value: "camera=(), microphone=(), geolocation=()",
+//           },
+//           {
+//             key: "Cross-Origin-Resource-Policy",
+//             value: "same-site",
+//           },
+//         ],
+//       },
+//     ];
+//   },
+// };
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['indigo-secret-viper-132.mypinata.cloud']
-    },
+  images: {
+    domains: ['indigo-secret-viper-132.mypinata.cloud'],
+  },
   async headers() {
     return [
       {
-        source: "/(.*)",
+        source: "/_next/image(.*)",
         headers: [
           {
-            key: "Content-Security-Policy",
-            value:
-              "default-src 'self'; " +
-              "script-src 'self' 'unsafe-inline'; " +
-              "object-src 'none'; " +
-              "frame-ancestors 'none'; " +
-              "frame-src https://www.google.com; " +
-              "style-src 'self' 'unsafe-inline'; " +
-              "img-src 'self' data: https:; " +
-              "font-src 'self' https: data:;",
+            key: "Access-Control-Allow-Origin",
+            value: "https://honra-health-center-front-xejz.vercel.app",
           },
+        ],
+      },
+      {
+        source: "/(.*)",
+        headers: [
           {
             key: "X-Frame-Options",
             value: "DENY",
@@ -42,7 +79,7 @@ const nextConfig = {
           },
           {
             key: "Cross-Origin-Resource-Policy",
-            value: "same-site",
+            value: "cross-origin",
           },
         ],
       },
