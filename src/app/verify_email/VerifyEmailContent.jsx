68123e4e8cef3b0f58a8e9aa49ae8f616ff0e590
Jiaks <160535015+Jiaks95@users.noreply.github.com>
2025-06-10 12:47:44 +0200

    Try to fix verify_email page render error

---
+++ src/app/verify_email/VerifyEmailContent.jsx
@@ -0,0 +1,37 @@
+"use client"
+
+import Image from "next/image"
+import { useSearchParams } from "next/navigation"
+import { useState, useEffect } from "react"
+
+export default function VerifyEmailContent() {
+    const searchParams = useSearchParams()
+    const token = searchParams.get("token")
+    const [message, setMessage] = useState("Verifying user, please wait.")
+
+    useEffect(() => {
+        if (!token) return setMessage("No token provided in the URL.")
+
+        const sendToken = async () => {
+            try {
+                const response = await fetch(`${process.env.API_PATH}/patients/verify-email?token=${token}`)
+                setMessage(response.ok
+                    ? "Email verified successfully. You can now log in."
+                    : "Email was not verified successfully. Please try again.")
+            } catch (err) {
+                setMessage("Something went wrong. Please try again later.")
+            }
+        }
+
+        sendToken()
+    }, [token])
+
+    return (
+        <section className="w-full h-screen flex items-center justify-center">
+            <div className="w-1/2 h-1/2 bg-[var(--turquoise)] rounded-3xl flex flex-col items-center justify-center">
+                <Image src={'/images/logo.png'} alt="logo" width={50} height={50}/>
+                <p className="font-bold text-[var(--outer_space)]">{message}</p>
+            </div>            
+        </section>
+    )
+}
